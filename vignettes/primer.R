011caf01fae82a22e22c6327d95c3d1d5252fcaa
drsimonj <drsimonjackson@gmail.com>
2016-03-22 11:56:44 +1100

    Added vignette for quick-start with survey setup

--- vignettes/primer.R
+++ vignettes/primer.R
@@ -2,10 +2,59 @@
 # Set global knitr options
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ---- eval = FALSE-------------------------------------------------------
+#  library(qrtenginer)
+#  d <- extractQrteGz("/path/to/YourParsedFile_out.csv.gz")
+#  write.csv(scoreQrte(d), "/path/to/save/your/testResults.csv", row.names = FALSE)
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ------------------------------------------------------------------------
+library(devtools)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  devtools::install_github("drsimonj/qrtenginer")
+
+## ------------------------------------------------------------------------
+system("python --version")
+
+## ------------------------------------------------------------------------
+library(qrtenginer)
+
+## ------------------------------------------------------------------------
+d <- parseQrte("/Users/Simon/Desktop/Flanker.zip",
+               "/Users/Simon/Desktop/qrteparser-webservice-master/parser.py")
+
+## ------------------------------------------------------------------------
+str(d)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  library(qrtenginer)
+#  d <- parseQrte("path/to/Qualtrics/data.zip", "path/to/QRTEngine/parser.py")
+
+## ------------------------------------------------------------------------
+library(qrtenginer)
+d <- extractQrteGz("/Users/Simon/Desktop/Flanker_out.csv.gz")
+
+## ------------------------------------------------------------------------
+str(d)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  library(qrtenginer)
+#  d <- extractQrteGz("/path/to/YourParsedFile_out.csv.gz")
+
+## ------------------------------------------------------------------------
+scoreQrte(d)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  write.csv(scoreQrte(d), "/Users/Simon/Desktop/Flanker_results.csv", row.names = FALSE)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  library(qrtenginer)
+#  d <- extractQrteGz("/path/to/YourParsedFile_out.csv.gz")
+#  write.csv(scoreQrte(d), "/path/to/save/your/testResults.csv", row.names = FALSE)
+
+## ------------------------------------------------------------------------
+unique(d$ResponseID)
+
+## ------------------------------------------------------------------------
+split(scoreQrte(d), scoreQrte(d)$ResponseID)
 
