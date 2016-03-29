44c4704d5b58256a8f41c9685a8dcc379d697159
drsimonj <drsimonjackson@gmail.com>
2016-03-29 15:05:11 +1100

    Added compatible survey description vignette. TBC

---
+++ R/qrtenginer.R
@@ -0,0 +1,22 @@
+#' qrtenginer: A package for parsing and scoring QRTEngine tests.
+#'
+#' The qrtenginer package provides two categories of important functions:
+#' parsing and scoring
+#'
+#' @section parsing functions:
+#' The parsing functions ...
+#'
+#' @section scoring functions:
+#' The scoring functions ...
+#'
+#' @section compatible surveys:
+#' A suite of tests have been built to be compatible with the scoring functions
+#' provided by the qrtengine package. There are two purposes for these tests
+#' being available. One is so that researchers can quickly deploy and analyse
+#' data without the burdens of programming. The second is to encourage
+#' researchers to work with a standard test suite so that suitable comparisons
+#' can be made across studies.
+#'
+#' @docType package
+#' @name qrtenginer
+NULL
